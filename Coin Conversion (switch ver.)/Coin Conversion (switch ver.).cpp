c4c96dbbad616fc213185c2011917a51fde76470
Ben Choi <bjchoi514@gmail.com>
2014-08-01 16:40:12 -0700

    Version 1.0

---
+++ Coin Conversion (switch ver.)/Coin Conversion (switch ver.).cpp
@@ -0,0 +1,12 @@
+// Coin Conversion (switch ver.).cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "std_lib_facilities.h"
+
+int main()
+{
+
+	return 0;
+}
+
